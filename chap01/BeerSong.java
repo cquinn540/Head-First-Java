f4f5d1cda06eb1487720898a71ef856d4b1a5d40
cquinn540 <cquinn540@gmail.com>
2017-11-28 15:33:42 -0600

    changed bottles of beer loop
    same

--- chap01/BeerSong.java
+++ chap01/BeerSong.java
@@ -6,15 +6,15 @@ public class BeerSong {
         String word = "bottles";
         while (beerNum > 0)
         {
-            if (beerNum == 1)
-            {
-                word = "bottle";
-            }
             System.out.println(beerNum + " " + word + " of beer on the wall");
             System.out.println(beerNum + " " + word + " of beer");
             System.out.println("Take one down.");
             System.out.println("Pass it around.");
             beerNum = beerNum - 1;
+			if (beerNum == 1)
+            {
+                word = "bottle";
+            }
             if (beerNum > 0)
             {
                 System.out.println(beerNum +  " " + word + " of beer on the wall");
